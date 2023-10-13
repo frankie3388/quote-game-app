3d1b8c2c346834d8a77c74582dad8af57c972027
“frankie3388” <frankiejuen33@gmail.com>
2023-10-13 23:18:47 +1000

    fix: put in error handling for search component

--- src/components/SearchQuote.jsx
+++ src/components/SearchQuote.jsx
@@ -17,19 +17,24 @@ export default function SearchQuote() {
 
     useEffect(() => {
         async function searchQuote() {
-            let response = await fetch(api + 'quotes?author=' + authorOfQuote)
+            try {
+                let response = await fetch(api + 'quotes?author=' + authorOfQuote)
+            
+                let responseData = await response.json();
 
-            let responseData = await response.json();
+                let quote = responseData.results[Math.floor(Math.random() * responseData.count)].content;
 
-            let quote = responseData.results[Math.floor(Math.random() * responseData.count)].content;
+                let author = responseData.results[0].author;
 
-            let author = responseData.results[0].author;
+                // toggle display for homepage (random quote or searched quote)
+                let toggleDisplay = toggle
 
-            // toggle display for homepage (random quote or searched quote)
-            let toggleDisplay = toggle
-
-            setQuoteData({quote, author, toggleDisplay});
-
+                setQuoteData({quote, author, toggleDisplay});
+            }
+            catch (error) {
+                console.error('Error fetching author information:', error);
+                alert("Cannot find Author")
+            }
         }
 
         searchQuote();
